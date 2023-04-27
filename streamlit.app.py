2fe440e2fc1ca04e42381d3e431737a477db40e9
thatsmydob <87033018+thatsmydob@users.noreply.github.com>
2023-04-27 13:50:21 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -58,7 +58,6 @@ if streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
-streamlit.stop()
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
